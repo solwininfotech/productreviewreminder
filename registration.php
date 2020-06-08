8c1f32a3a14420052e237036c7f6348c0db7c34a
Mayur Dhudasia <mmdhudasia@gmail.com>
2020-06-08 15:01:07 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_ProductReviewReminder',
+    __DIR__
+);
